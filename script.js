d705aea51d69e948e554ef36cbbf68b35aebf676
nwataghana-lang <nwataghana@gmail.com>
2025-08-02 20:01:57 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let count = 0;
-const button = document.getElementById("tap-btn");
-const counter = document.getElementById("counter");
-
-button.addEventListener("click", () => {
-  count++;
-  counter.textContent = `Taps: ${count}`;
-});
